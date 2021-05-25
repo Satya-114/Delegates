6b09317bafcb545be787d028635ebcfd49cbd40d
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-25 21:35:02 +0530

    Add files via upload

---
+++ Multicast/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateEX
+{
+    public delegate void mydele(int x, int y);
+
+    class task
+    {
+        public void add(int x, int y)
+        {
+            Console.WriteLine("The Sum is " + (x + y));
+        }
+
+        public void sub(int x, int y)
+        {
+            Console.WriteLine("The Difference is " + (x - y));
+            Console.ReadKey();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            task  b1 = new task  ();
+            mydele Md = new mydele(b1.add);
+            Md += b1.sub;
+            Md(20, 10);
+
+
+        }
+    }
+}
