3e24ec0899825f33a0260a043b5f5e46632b33f9
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-25 21:30:37 +0530

    Add files via upload

---
+++ single cast/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Single_cast_1
+{
+    public delegate void mydele(int x, int y);
+
+    class A
+    {
+        public void add(int x, int y)
+        {
+            Console.WriteLine("The Sum is " + (x + y));
+        }
+
+        public void sub(int x, int y)
+        {
+            Console.WriteLine("The Difference is " + (x - y));
+            Console.ReadKey();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A();
+            
+            mydele m = new mydele(obj.add);
+            m(10, 20);
+            mydele m1 = new mydele(obj.sub);
+            m1(10, 20);
+        }
+    }
+}
